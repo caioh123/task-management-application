f47a94b86e3a8326fd1d8e6b7a2ded9e7080db9a
Caio Novaes <caionwebdev@gmail.com>
2021-07-16 10:22:28 -0300

    postgres

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,6 +1,7 @@
-import { Body, Controller, Delete, Get, Param, Patch, Post, Query } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post, Query, UsePipes, ValidationPipe } from '@nestjs/common';
 import { CreateTaskDto } from './dto/create-task.dto';
 import { GetTasksFilterDto } from './dto/get-tasks-filter.dto';
+import { TaskStatusValidationPipe } from './pipes/task-status-validation.pipe';
 import { Task, TaskStatus } from './task.model';
 import { TasksService } from './tasks.service';
 
@@ -9,9 +10,12 @@ export class TasksController {
   constructor(private tasksService: TasksService) { }
 
   @Get()
-  getTasks(@Query() filterDto: GetTasksFilterDto): Task[] {
-    console.log(filterDto)
-    return this.tasksService.getAllTasks()
+  getTasks(@Query(ValidationPipe) filterDto: GetTasksFilterDto): Task[] {
+    if(Object.keys(filterDto).length) {
+      return this.tasksService.getTasksWithFilters(filterDto)
+    } else {
+      return this.tasksService.getAllTasks()
+    }
   }
 
   @Get("/:id")
@@ -25,16 +29,15 @@ export class TasksController {
   }
 
   @Post()
+  @UsePipes(ValidationPipe)
   createTask(@Body() createTaskDto: CreateTaskDto): Task {
-
     return this.tasksService.createTask(createTaskDto)
-
   }
 
   @Patch("/:id/status")
   updateTaskStatus(
     @Param("id") id: string,
-    @Body("status") status: TaskStatus
+    @Body("status", TaskStatusValidationPipe) status: TaskStatus
   ): Task {
     return this.tasksService.updateTaskStatus(id, status)
   }
--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,7 +1,8 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { Task, TaskStatus } from './task.model';
 import { v4 as uuidv4 } from 'uuid'
 import { CreateTaskDto, UpdateTaskDto } from './dto/create-task.dto';
+import { GetTasksFilterDto } from './dto/get-tasks-filter.dto';
 
 @Injectable()
 export class TasksService {
@@ -11,12 +12,38 @@ export class TasksService {
     return this.tasks
   }
 
+  getTasksWithFilters(filterDto: GetTasksFilterDto): Task[] {
+    const {status, search} = filterDto
+
+    let tasks = this.getAllTasks()
+
+    if(status) {
+      tasks = tasks.filter(task => task.status === status)
+    }
+
+    if(search) {
+      tasks = tasks.filter(task => 
+        task.title.includes(search) ||
+        task.description.includes(search)
+      )
+    }
+
+    return tasks;
+  }
+
   getTaskById(id: string): Task {
-    return this.tasks.find((task) => task.id === id)
+    const found = this.tasks.find((task) => task.id === id)
+
+    if(!found) {
+      throw new NotFoundException(`Task with ID "${id}" not found`)
+    }
+
+    return found
   }
 
   deleteTaskById(id: string): void {
-    this.tasks = this.tasks.filter((task) => task.id !== id)
+    const found = this.getTaskById(id)
+    this.tasks = this.tasks.filter((task) => task.id !== found.id)
   }
 
   createTask(CreateTaskDto: CreateTaskDto): Task {
